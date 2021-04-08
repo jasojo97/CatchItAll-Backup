a27c88673b86dc97c46a9be21da08a06a827b135
Jason Plourde <jasojo97@gmail.com>
2021-04-08 16:15:06 -0700

    "Building cartSummary component"

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import {VendorRepository} from "./vendor.repository";
 import {StaticDataSource} from "./static.datasource";
+import {Cart} from "./cart.model";
 
 
 @NgModule({
-    providers: [VendorRepository, StaticDataSource]
+    providers: [VendorRepository, StaticDataSource, Cart]
 })
 
 export class ModelModule { }
